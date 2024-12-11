ecaa8088b55dba165b8230a5efbf6a9d3481a616
Akinola Akinjide <akinjide19@gmail.com>
2024-12-11 08:52:05 -0500

    add new file

---
+++ app.js
@@ -0,0 +1,12 @@
+
+function handleClose(){
+    const displayToggle = document.querySelector('.sidebar')
+    displayToggle.style.display = "none"
+}
+
+
+function showSideBar(){
+    const displayToggle = document.querySelector('.sidebar')
+    displayToggle.style.display = "block"
+  console.log('clicked')
+}
